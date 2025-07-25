afc932e5015f2b82cd2e61c5e92153815c3e83e1
seongyooo <seonlineseon@gmail.com>
2025-07-25 15:23:55 +0900

    feat: tcp & udp & thread create

---
+++ server/final_project/test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <pthread.h>
+#include <netinet/in.h>
+
+int main(int argc, char *argv[]){
+    struct ip_mreq join_adr;
+
+    return 0;
+}
